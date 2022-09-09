8bb1dac2452b077ab05497cde928a5a1e7219b3b
Vitaliy Zhyryk <80642298+WindyBoss@users.noreply.github.com>
2022-09-09 13:27:45 +0200

    Update Contacts.jsx

--- src/feature/Contacts/Contacts.jsx
+++ src/feature/Contacts/Contacts.jsx
@@ -12,7 +12,7 @@ function Contacts() {
   const { data, isLoading, isSuccess, isError, error } = useGetContactsQuery();
 
   useEffect(() => {
-    data && setContacts(data);
+    setContacts(data);
   }, [data]);
 
   let context = '';
